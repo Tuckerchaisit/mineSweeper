3bce3066d4420679b9b0f9eee830e6406491384f
Tucker <tucker.chaisit@gmail.com>
2022-02-16 15:58:43 -0500

    add board

---
+++ js/app.js
@@ -0,0 +1,86 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let board;
+let bombCounts;
+let flagCounts;
+let counter=0;
+
+
+/*------------------------ Cached Element References ------------------------*/
+const sq = Array.from(document.querySelectorAll(".sq"))
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+sq.forEach( (square,index) =>{
+  square.addEventListener('click',() => handleClick(index));
+  }
+)
+
+
+/*-------------------------------- Functions --------------------------------*/
+init();
+
+
+function init(){
+  board = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+  bombCounts = 21;
+  placeBomb();
+  showBomb();
+  render();
+  console.log(board)
+  console.log(counter);
+}
+
+function render(){
+  
+}
+function placeBomb(){ //while numBombs is more than 0 then place bomb randomly on the board until numBombs is 0// bomb is equal to -1
+  let probability;
+  while(bombCounts>0){
+    for(let i=0; i<board.length; i++){
+      probability=Math.random();
+      //console.log(probability);
+      if(probability<0.0476190476 && board[i]===null){
+        board[i]=-1;
+        bombCounts--;
+        console.log(bombCounts);
+        if(bombCounts===0){
+          break;
+        }
+      }
+    }
+  }
+}
+
+function showBomb(){
+  for(let i=0; i<board.length; i++){
+    if(board[i]===-1){
+      sq[i].textContent='💣';
+      counter++;
+    }
+  } 
+}
+
+function handleClick(index){
+  
+}
+
+function checkAdjacent(index){
+
+}
+
+function isTopEdge(index){
+
+}
+function isLeftEdge(index){
+  
+}
+function isLeftEdge(index){
+
+}
+function isLeftEdge(index){
+
+}
